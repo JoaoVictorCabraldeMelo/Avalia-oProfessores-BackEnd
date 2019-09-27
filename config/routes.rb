795e117533b9869b6be727207c0e276fba2092a3
clarice42 <claramaia820@gmail.com>
2019-09-26 22:17:38 -0300

    crud de usuario feito

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Rotas para o usuario
+  get "/usuarios" => "users#index"
+  get "/usuario/:id" => "users#show"
+  post "/cadastrar" => "users#create"
+  put "/usuario/atualizar/:id" => "users#update"
+  delete "/usuario/deletar/:id" => "users#delete"
+
 end
